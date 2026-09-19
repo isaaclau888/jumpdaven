4728d78300b86855c9898f3d20fd4aa40f0f3d1c
isaaclau888 <isaaclau888@gmail.com>
2026-09-19 23:53:25 +0800

    Fix end game button scene paths and remove git plugin

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_play_again_button_pressed():
-	get_tree().change_scene_to_file("res://scripts/world.gd")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_menu_button_pressed():
-	get_tree().change_scene_to_file("res://scripts/main_menu.gd")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
